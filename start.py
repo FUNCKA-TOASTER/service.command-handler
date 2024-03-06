3fd92b5e85a72c4d18601e5712731769a7b2a995
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-03-06 11:12:43 +1000

    Entry point

---
+++ start.py
@@ -0,0 +1,19 @@
+"""Service "toaster.comman-handling-service".
+About:
+    ...
+    
+Author:
+    Oidaho (Ruslan Bashinskii)
+    oidahomain@gmail.com
+"""
+import asyncio
+
+
+async def main():
+    """Entry point.
+    """
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
