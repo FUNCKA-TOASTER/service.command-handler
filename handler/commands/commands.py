4a43d08b1434483d027bc234dfb19b48ad7300df
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-03-08 23:58:50 +1000

    New commands

--- handler/commands/commands.py
+++ handler/commands/commands.py
@@ -14,11 +14,11 @@ class TestCommand(BaseCommand):
         Keyboard
         e.t.c
     """
-    COMMAND_NAME = "test"
-    _permission_lvl = 2
+    _COMMAND_NAME = "test"
+    _PERMISSION_LVL = 2
 
     async def _handle(self, event: dict, kwargs) -> bool:
-        answer_text = f"Вызвана комманда <{self.COMMAND_NAME}> " \
+        answer_text = f"Вызвана комманда <{self.name}> " \
                       f"с аргументами {kwargs.get('argument_list')}."
 
         keyboard = (
@@ -42,6 +42,169 @@ class TestCommand(BaseCommand):
                     label="Негатив",
                     payload={
                         "call_action": "negative_test"
+                    }
+                ),
+                ButtonColor.NEGATIVE
+            )
+        )
+
+        self.api.messages.send(
+            peer_id=event.get("peer_id"),
+            random_id=0,
+            message=answer_text,
+            keyboard=keyboard.json
+        )
+
+        return True
+
+
+
+class MarkCommand(BaseCommand):
+    """Mark command.
+    Initializes conversation marking process.
+    Allows:
+        - To mark conversation as "CHAT" or "LOG".
+        - Update the data about conversation.
+        - Delete conversation mark.
+    """
+    _COMMAND_NAME = "mark"
+    _PERMISSION_LVL = 2
+
+    async def _handle(self, event: dict, kwargs) -> bool:
+        answer_text = "⚠️ Вы хотите пометить новую беседу? \n\n" \
+        "Выберите необходимое дествие из меню ниже:"
+
+        keyboard = (
+            Keyboard(
+                inline=True,
+                one_time=False,
+                owner_id=event.get("user_id")
+            )
+            .add_row()
+            .add_button(
+                Callback(
+                    label="CHAT",
+                    payload={
+                        "call_action": "mark_as_chat"
+                    }
+                ),
+                ButtonColor.POSITIVE
+            )
+            .add_button(
+                Callback(
+                    label="LOG",
+                    payload={
+                        "call_action": "mark_as_log"
+                    }
+                ),
+                ButtonColor.POSITIVE
+            )
+            .add_row()
+            .add_button(
+                Callback(
+                    label="Обновить данные беседы",
+                    payload={
+                        "call_action": "update_conv_data"
+                    }
+                ),
+                ButtonColor.SECONDARY
+            )
+            .add_row()
+            .add_button(
+                Callback(
+                    label="Сбросить метку",
+                    payload={
+                        "call_action": "drop_mark"
+                    }
+                ),
+                ButtonColor.NEGATIVE
+            )
+            .add_button(
+                Callback(
+                    label="Отмена команды",
+                    payload={
+                        "call_action": "cancel_command"
+                    }
+                ),
+                ButtonColor.NEGATIVE
+            )
+        )
+
+        self.api.messages.send(
+            peer_id=event.get("peer_id"),
+            random_id=0,
+            message=answer_text,
+            keyboard=keyboard.json
+        )
+
+        return True
+
+
+
+class PermissionCommand(BaseCommand):
+    """Permission command.
+    Sets new permission role to user.
+    Allows:
+        - Set "Administrator" role.
+        - Set "Moderator" role.
+        - Set "User" role.
+    """
+    _COMMAND_NAME = "permission"
+    _PERMISSION_LVL = 2
+
+    async def _handle(self, event: dict, kwargs) -> bool:
+        user_tag = kwargs.get('argument_list')[0]
+
+        if not self.is_tag(user_tag):
+            return False
+
+        answer_text = f"⚠️ Уровни доступа пользователя {user_tag} \n\n" \
+        "Выберите необходимое дествие из меню ниже:"
+
+        keyboard = (
+            Keyboard(
+                inline=True, 
+                one_time=False,
+                owner_id=event.get("user_id")
+            )
+            .add_row()
+            .add_button(
+                Callback(
+                    label="Модератор",
+                    payload={
+                        "call_action": "set_moderator_permission",
+                        "target": self.id_from_tag(user_tag)
+                    }
+                ),
+                ButtonColor.POSITIVE
+            )
+            .add_button(
+                Callback(
+                    label="Администратор",
+                    payload={
+                        "call_action": "set_administrator_permission",
+                        "target": self.id_from_tag(user_tag)
+                    }
+                ),
+                ButtonColor.POSITIVE
+            )
+            .add_button(
+                Callback(
+                    label="Пользователь",
+                    payload={
+                        "call_action": "set_user_permission",
+                        "target": self.id_from_tag(user_tag)
+                    }
+                ),
+                ButtonColor.NEGATIVE
+            )
+            .add_row()
+
+            .add_button(
+                Callback(
+                    label="Отмена команды",
+                    payload={
+                        "call_action": "cancel_command"
                     }
                 ),
                 ButtonColor.NEGATIVE
