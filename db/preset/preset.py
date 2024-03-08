32c1dedecc98eb5d8dce6773856a2c1a882911fc
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-03-08 19:04:54 +1000

    New presets

--- db/preset/preset.py
+++ db/preset/preset.py
@@ -1,3 +1,9 @@
+from .tables import (
+    toaster_tables,
+    toaster_settings_tables
+)
+
+
 class Preseter(object):
     """Cool presetter who knows how to do some things
     actions on the default MySQL database by calling
@@ -8,4 +14,20 @@ class Preseter(object):
         self.con = connection
         self.cur = cursor
 
+
     # add custom presets
+    def deploy_toaster(self):
+        """docstring
+        """
+        self.cur.execute('CREATE DATABASE IF NOT EXISTS toaster')
+        self.cur.execute('CREATE DATABASE IF NOT EXISTS toaster_settings')
+
+        for query in toaster_tables:
+            self.cur.execute(query)
+
+        for query in toaster_settings_tables:
+            self.cur.execute(query)
+
+
+    def conv_settings(self, conv_id: int):
+        pass
