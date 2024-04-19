b9394ffa24fb3f9b30f578c883a419c4deec04d3
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-04-19 13:13:08 +1000

    Added producer

---
+++ producer/custom.py
@@ -0,0 +1,33 @@
+"""Module "producer"."""
+
+from .body import Producer
+
+
+class CustomProducer(Producer):
+    """Custom producer class.
+    Preferences for implimentation of custom
+    functions for working with data that needs
+    to be pushed into a queue inside RabbitMQ.
+    """
+
+    event_queues = {
+        "warn": "warns",
+    }
+
+    async def initiate_warn(self, event, warns, target, target_name):
+        queue = self.event_queues["warn"]
+        data = {
+            "author_id": event.get("user_id"),
+            "author_name": event.get("user_name"),
+            "reason_message": "Модератор вынес вам предупреждение!",
+            "setting": None,
+            "target_id": target,
+            "target_name": target_name,
+            "peer_id": event.get("peer_id"),
+            "cmid": event.get("cmid"),
+            "warn_count": warns,
+        }
+        await self._send_data(data, queue)
+
+
+producer = CustomProducer()
