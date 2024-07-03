8800dfd8f554c990ab5475059201b750c768f875
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-03 22:12:00 +1000

    annotated

--- data/annotated.py
+++ data/annotated.py
@@ -4,7 +4,7 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.mysql import BIGINT
 
 # TODO: Комментарий
-BPID = Annotated[int, mapped_column(BIGINT, ForeignKey("peers.bpid"), primary_key=True)]
+BPID = Annotated[int, mapped_column(BIGINT, ForeignKey("peers.id"), primary_key=True)]
 
 # TODO: Комментарий
-UUID = Annotated[int, mapped_column(BIGINT, ForeignKey("users.uuid"), primary_key=True)]
+UUID = Annotated[int, mapped_column(BIGINT, primary_key=True)]
