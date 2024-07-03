8b702721af6f8877e84c8f2ae54786ff8c670043
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-03 22:10:35 +1000

    Basemodel

--- toaster/database/database.py
+++ toaster/database/database.py
@@ -7,27 +7,35 @@ About:
     ...
 """
 
-from typing import NoReturn, Any
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from typing import Any
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import Session, sessionmaker, declarative_base
+
+BaseModel = declarative_base()
 
 
 class Database:
     """DOCSTRING"""
 
-    def __init__(self, connection_uri: str, debug: bool = False):
-        self._engine = create_engine(connection_uri)
+    def __init__(self, connection_uri: str, debug: bool = False) -> None:
+        self._engine = create_engine(connection_uri, echo=debug)
         self._session = sessionmaker(bind=self._engine, autoflush=False)
 
-    def create_tables(self, base: Any) -> NoReturn:
+    def site_create_tables(self, base: Any) -> None:
         base.metadata.create_all(self._engine)
 
-    def drop_tables(self, base: Any) -> NoReturn:
+    def site_drop_tables(self, base: Any) -> None:
         base.metadata.drop_all(self._engine)
 
+    def create_tables(self) -> None:
+        BaseModel.metadata.create_all(self._engine)
+
+    def drop_tables(self) -> None:
+        BaseModel.metadata.create_all(self._engine)
+
     @property
-    def engine(self):
+    def engine(self) -> Engine:
         return self._engine
 
-    def make_session(self):
-        return self.session()
+    def make_session(self) -> Session:
+        return self._session()
