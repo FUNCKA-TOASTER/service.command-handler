b146f62bad38113d93d1e9e7a3bc8b52cf21443d
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-04 17:39:23 +1000

    Fuinction overload

--- toaster/database/database.py
+++ toaster/database/database.py
@@ -8,8 +8,10 @@ About:
 """
 
 from typing import Any
+from functools import singledispatch
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import Session, sessionmaker, declarative_base
+
 
 BaseModel = declarative_base()
 
@@ -21,17 +23,21 @@ class Database:
         self._engine = create_engine(connection_uri, echo=debug)
         self._session = sessionmaker(bind=self._engine, autoflush=False)
 
-    def site_create_tables(self, base: Any) -> None:
-        base.metadata.create_all(self._engine)
-
-    def site_drop_tables(self, base: Any) -> None:
-        base.metadata.drop_all(self._engine)
-
+    @singledispatch
     def create_tables(self) -> None:
         BaseModel.metadata.create_all(self._engine)
 
+    @singledispatch
     def drop_tables(self) -> None:
         BaseModel.metadata.create_all(self._engine)
+
+    @create_tables.register
+    def _(self, base: Any):
+        base.metadata.create_all(self._engine)
+
+    @drop_tables.register
+    def _(self, base: Any):
+        base.metadata.drop_all(self._engine)
 
     @property
     def engine(self) -> Engine:
