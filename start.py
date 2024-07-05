25e70ae54794ed83e94345fe3c122bec8949391e
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-05 17:00:05 +1000

    Loguru

--- start.py
+++ start.py
@@ -11,24 +11,31 @@ About:
     command name specified in the event text.
 """
 
+import sys
+from loguru import logger
 from toaster.broker import Subscriber, build_connection
-from toaster.logger import Logger
-
-# from handler import CommandHandler
+from handler import CommandHandler
 import config
+
+
+def setup_logger() -> None:
+    logger.remove()
+    logger.add(
+        sys.stdout,
+        colorize=True,
+        format="<red>{module}</red> | <green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | {message}",
+        level="DEBUG",
+    )
 
 
 def main():
     """Entry point."""
 
     subscriber = Subscriber(client=build_connection(config.REDIS_CREDS))
-    logger = Logger()
-    # handler = CommandHandler()
+    handler = CommandHandler()
 
-    logger.info("Waiting for events...")
     for event in subscriber.listen(channel_name=config.CHANNEL_NAME):
-        logger.info(f"Recived new event: \n{event}")
-        # handler(event)
+        handler(event)
 
 
 if __name__ == "__main__":
