879394af3ec0e5b6c9ef12a15adcef6416a8d925
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-05 21:24:29 +1000

    Test Command

--- commands/commands.py
+++ commands/commands.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
 from toaster.broker.events import Event
+from rules import requires_mark, requires_permission
 from .base import BaseCommand
 
 
@@ -9,4 +10,12 @@ class TestCommand(BaseCommand):
     NAME = "test"
 
     def _handle(self, event: Event, args: Optional[List[str]]) -> bool:
-        pass
+        answer_text = "⚠️ Тестовая команда!"
+
+        answer = self.api.messages.send(
+            peer_ids=event.peer.bpid,
+            random_id=0,
+            message=answer_text,
+        )
+
+        return True
