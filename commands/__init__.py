a4cadce97bddb6d5cacdd3a8991d840f6159f29d
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-05 20:40:32 +1000

    Commands reworked

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,40 +1,10 @@
 from .commands import (
-    MarkCommand,
-    PermissionCommand,
-    GameCommand,
-    SayCommand,
-    DeleteCommand,
-    CopyCommand,
-    SettingsCommand,
-    DelayCommand,
-    KickCommand,
-    AddCurseWordCommand,
-    AddURLFilterPatternCommand,
-    ExpireCommand,
-    WarnCommand,
-    PunishmentCommand,
-    ProfileCommand,
-    UnwarnCommand,
+    TestCommand,
 )
 
 
 command_list = {
-    PermissionCommand.NAME: PermissionCommand,
-    MarkCommand.NAME: MarkCommand,
-    GameCommand.NAME: GameCommand,
-    SayCommand.NAME: SayCommand,
-    DeleteCommand.NAME: DeleteCommand,
-    CopyCommand.NAME: CopyCommand,
-    SettingsCommand.NAME: SettingsCommand,
-    DelayCommand.NAME: DelayCommand,
-    KickCommand.NAME: KickCommand,
-    AddCurseWordCommand.NAME: AddCurseWordCommand,
-    AddURLFilterPatternCommand.NAME: AddURLFilterPatternCommand,
-    ExpireCommand.NAME: ExpireCommand,
-    WarnCommand.NAME: WarnCommand,
-    PunishmentCommand.NAME: PunishmentCommand,
-    ProfileCommand.NAME: ProfileCommand,
-    UnwarnCommand.NAME: UnwarnCommand,
+    TestCommand.name: TestCommand,
 }
 
 
--- commands/base.py
+++ commands/base.py
@@ -1,18 +1,28 @@
 import re
+from typing import Union, Optional, Tuple, List
+from abc import ABC, abstractmethod
 from vk_api import VkApi
-from db import db
-from .abc import ABCHandler
+from toaster.broker.events import Event
 
 
-class BaseCommand(ABCHandler):
-    """Command handler base class."""
+class BaseCommand(ABC):
+    """DOCSTRING"""
 
-    PERMISSION = 0
     NAME = "None"
-    MARK = ()
 
     def __init__(self, api: VkApi):
         self.api = api
+
+    def __call__(self, name: str, args: Optional[List[str]], event: Event) -> bool:
+        return self._handle(name, args, event)
+
+    @abstractmethod
+    def _handle(self, name: str, args: Optional[List[str]], event: Event) -> bool:
+        """DOCSTRING"""
+
+    @property
+    def name(self):
+        return self.name
 
     @staticmethod
     def is_tag(tag: str) -> bool:
@@ -24,36 +34,34 @@ class BaseCommand(ABCHandler):
         sep_pos = tag.find("|")
         return int(tag[3:sep_pos])
 
-    def name_from_id(self, user_id) -> str:
-        user_info = self.api.users.get(user_ids=user_id, fields=["domain"])
+    def name_from_id(self, uuid: Union[str, int]) -> Optional[Tuple[str, str]]:
+        user_info = self.api.users.get(user_ids=uuid, fields=["domain"])
 
         if not user_info:
             return None
 
-        user_name = " ".join(
-            [user_info[0].get("first_name"), user_info[0].get("last_name")]
-        )
-
-        return user_name
+        return (user_info[0].get("first_name"), user_info[0].get("last_name"))
 
     def initiate_session(self, conv_id: int, cmid: int) -> None:
-        interval = db.execute.select(
-            schema="toaster_settings",
-            table="delay",
-            fields=("delay",),
-            conv_id=conv_id,
-            setting_name="menu_session",
-        )
+        pass
+        # interval = db.execute.select(
+        #     schema="toaster_settings",
+        #     table="delay",
+        #     fields=("delay",),
+        #     conv_id=conv_id,
+        #     setting_name="menu_session",
+        # )
 
-        interval = int(interval[0][0]) if interval else 0
-        query = f"""
-        INSERT INTO 
-            menu_sessions(conv_id, cm_id, expired)
-        VALUES 
-	        (
-                '{conv_id}',
-                '{cmid}',
-                NOW() + INTERVAL {interval} MINUTE
-            );
-        """
-        db.execute.raw(schema="toaster", query=query)
+        # interval = int(interval[0][0]) if interval else 0
+        # query = f"""
+        # INSERT INTO
+        #     menu_sessions(conv_id, cm_id, expired)
+        # VALUES
+
+        #     (
+        #         '{conv_id}',
+        #         '{cmid}',
+        #         NOW() + INTERVAL {interval} MINUTE
+        #     );
+        # """
+        # db.execute.raw(schema="toaster", query=query)
