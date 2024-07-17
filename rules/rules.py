ff0701b3472299911e2798852ab6b4fc69d8d526
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-17 21:25:35 +1000

    Values fixed

--- rules/rules.py
+++ rules/rules.py
@@ -1,5 +1,6 @@
 from typing import List, Optional, Iterable
 from toaster.broker.events import Event
+from data import UserPermission, PeerMark
 from data import TOASTER_DB
 from data.scripts import (
     get_peer_mark,
@@ -7,7 +8,7 @@ from data.scripts import (
 )
 
 
-def requires_permission(permission_lvl: int):
+def requires_permission(permission_lvl: UserPermission):
     """DOCSTRING"""
 
     exception_message = "Access rejected. Low level of access rights."
@@ -17,7 +18,7 @@ def requires_permission(permission_lvl: int):
 
             def wrapper(name: str, args: Optional[List[str]], event: Event):
                 user_permission = get_user_permission(TOASTER_DB, event)
-                if user_permission >= permission_lvl:
+                if user_permission >= permission_lvl.value:
                     return obj(name, args, event)
 
                 else:
@@ -42,7 +43,7 @@ def requires_permission(permission_lvl: int):
     return decorator
 
 
-def requires_mark(peer_mark: str):
+def requires_mark(peer_mark: PeerMark):
     """DOCSTRING"""
 
     exception_message = "Command execution aborted. Wrong peer mark."
@@ -52,7 +53,7 @@ def requires_mark(peer_mark: str):
 
             def wrapper(name: str, args: Optional[List[str]], event: Event):
                 mark = get_peer_mark(TOASTER_DB, event)
-                if mark == peer_mark:
+                if mark == peer_mark.value:
                     return obj(name, args, event)
 
                 else:
