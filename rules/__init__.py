bb4e60b9f9e68bc87fc3773cad518b7283c77d1a
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-18 16:10:02 +1000

    Rules updated

--- rules/__init__.py
+++ rules/__init__.py
@@ -2,13 +2,11 @@ from .rules import (
     requires_mark,
     requires_permission,
     requires_attachments,
-    requires_attachments,
 )
 
 
 __all__ = (
     "requires_mark",
     "requires_permission",
-    "requires_attachments",
     "requires_attachments",
 )
