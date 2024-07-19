4645677e44dcdaf33edf27ad70bcb5f5d29794a6
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-19 19:55:50 +1000

    cascade operations

--- data/annotated.py
+++ data/annotated.py
@@ -4,7 +4,14 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.mysql import BIGINT
 
 # TODO: Комментарий
-BPID = Annotated[int, mapped_column(BIGINT, ForeignKey("peers.id"), primary_key=True)]
+BPID = Annotated[
+    int,
+    mapped_column(
+        BIGINT,
+        ForeignKey("peers.id", ondelete="CASCADE", onupdate="CASCADE"),
+        primary_key=True,
+    ),
+]
 
 # TODO: Комментарий
 UUID = Annotated[int, mapped_column(BIGINT, primary_key=True)]
